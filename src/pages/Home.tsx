0ca25e1fc5980c3a5882c9b337e2b02ff5d04dc6
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-06-12 16:36:29 -0400

    feat: CategoryGrid debajo de Eventos destacados en Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,11 +26,8 @@ export default function Home() {
     <div className="min-h-screen pt-16" style={{ backgroundColor: '#FFFFFF' }}>
       <HeroSearch />
 
-      <CategoryGrid />
-
       {/* Eventos destacados */}
-      <section className="max-w-7xl mx-auto px-4 pb-20">
-        {/* Encabezado centrado */}
+      <section className="max-w-7xl mx-auto px-4 pt-12 pb-12">
         <div className="text-center mb-8">
           <h2 className="text-3xl font-bold tracking-tight" style={{ color: '#1D1D1F' }}>
             Eventos destacados
@@ -40,7 +37,6 @@ export default function Home() {
           </p>
         </div>
 
-        {/* Carrusel */}
         <div className="relative">
           <div
             ref={carouselRef}
@@ -54,7 +50,6 @@ export default function Home() {
             ))}
           </div>
 
-          {/* Flecha siguiente */}
           {events.length > 4 && (
             <button
               type="button"
@@ -68,7 +63,6 @@ export default function Home() {
           )}
         </div>
 
-        {/* Ver todos */}
         <div className="text-center mt-8">
           <Link
             to="/categorias"
@@ -80,13 +74,7 @@ export default function Home() {
         </div>
       </section>
 
-
-
-
-
-  
-
-
+      <CategoryGrid />
     </div>
   );
 }
